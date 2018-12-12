2d68f6afce41dea90c5c49ee30a4375b1237172f
yuriy_beloff <paganismrus@gmail.com>
2018-12-12 21:21:31 +0300

    modified search

--- vulners/vulners_engine/views.py
+++ vulners/vulners_engine/views.py
@@ -6,17 +6,20 @@ import requests
 import json
 
 
-def get_vulners_info(vendor, vulner, skip):
+def get_vulners_info(vendor=None, vulner=None, skip=None):
     params = {
         'query': 'type:{}'.format(vendor),
-        #'id': vulner,
+        'id': '"{}"'.format(vulner),
         'skip': int(skip)
 
     }
     vulners = requests.get(
-        'https://vulners.com/api/v3/search/lucene/', params=params).json()
+        'https://vulners.com/api/v3/search/lucene/', params=params)
+    print(vulners.url)
 
-    return  vulners #json.loads(vulners)
+    return  vulners.json() #json.loads(vulners)
+
+
 
 
 class VulnersListView(generic.ListView):
@@ -28,44 +31,29 @@ class VulnersListView(generic.ListView):
         increment = 20
         vendor = None
         vulner = None
-        # if not vendor:
-        #     return render(request, 'vulners_list.html', context={'form': form})
-        # try:
+
         if request.GET.get('skip', 0):
             skip = int(request.GET.get('skip', 0))
             skip += increment
-            # else:
-            #     skip = int(request.GET.get('skip', 20))
-            #     skip += increment
-                # int(request.GET['skip_prev'])
-                # skip -= increment
-        else: # KeyError:
-            # print(request.GET, 'HERERERE')
+            print('HERE_1')
+        else:
             skip = int(request.GET.get('skip_prev', 0))
-                # skip = 0
-            # else:
-                # skip = int(request.GET.get('skip_prev', 0))
             skip -= increment
             if skip <= 0:
                 skip = 0
-        # except MultiValueDictKeyError:
-        #     skip = 0
+            print('HERE_2')
         print(form)
         print('{} {} {}'.format(vendor, vulner, skip))
-        #     print('SKIP FIRST {}'.format(request.GET['skip'][0]))
         if form.is_valid():
             filters = form.cleaned_data
             print('FILTERS ARE: {}'.format(filters))
             vendor = filters['vendor']
             vulner = filters['vulner']
         vulners = get_vulners_info(vendor, vulner, skip)
-        # print(vulners['data']['search'][0]['_source']['id'])
         vln_lst = []
         if vulners:
             for i in vulners['data']['search']:
                 vln_lst.append(i['_source'])
-        print(request.GET)
-        print('SKIP {}'.format(skip))
         return render(request, 'vulners_list.html', context={'form': form,
                         'vulners_list': vln_lst,
                         'skip': skip})
