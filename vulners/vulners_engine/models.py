e435908b184a3c89c5bde76cae731995933ae162
YuriyB <paganism@mail.ru>
2018-12-15 16:14:01 +0300

    added db insert (only unique)

--- vulners/vulners_engine/models.py
+++ vulners/vulners_engine/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Vulner(models.Model):
     vendor = models.CharField(max_length=100)
-    vulner = models.CharField(max_length=100)
+    vulner = models.CharField(max_length=100, unique=True)
     description = models.TextField(max_length=5000)
     published = models.CharField(max_length=50)
 
--- vulners/vulners_engine/views.py
+++ vulners/vulners_engine/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import FilterForm
 from django.views import generic
 from .models import Vulner
+from django.core.exceptions import ObjectDoesNotExist
 
 import requests
 import json
@@ -27,7 +28,7 @@ class VulnersListView(generic.ListView):
         increment = 20
         vendor = None
         vulner = None
-
+        print('REQUEST POST {}'.format(request.POST))
         if request.GET.get('skip', 0):
             skip = int(request.GET.get('skip', 0))
             skip += increment
@@ -55,22 +56,49 @@ class VulnersListView(generic.ListView):
                                     'vulners_list': vln_lst,
                                     'skip': skip})
 
+
+
+
     def post(self, request):
-        print('HERE_POST')
-        form = FilterForm(request.POST)
+        form = FilterForm(request.POST or None)
+        increment = 20
+        vendor = None
+        vulner = None
+        print('REQUEST POST {}'.format(request.POST))
+        if request.GET.get('skip', 0):
+            skip = int(request.GET.get('skip', 0))
+            skip += increment
+            print('HERE_1')
+        else:
+            skip = int(request.GET.get('skip_prev', 0))
+            skip -= increment
+            if skip <= 0:
+                skip = 0
+            print('HERE_2')
+        print(form)
+        print('{} {} {}'.format(vendor, vulner, skip))
         if form.is_valid():
             filters = form.cleaned_data
             print('FILTERS ARE: {}'.format(filters))
             vendor = filters['vendor']
             vulner = filters['vulner']
-        vulners = get_vulners_info(vendor, vulner)
+        vulners = get_vulners_info(vendor, vulner, skip)
         vln_lst = []
         if vulners:
             for i in vulners['data']['search']:
                 vln_lst.append(i['_source'])
-                print(i['_source'])
-                Vulner(vendor=i['_source']['type'], 
-                                vulner=i['_source']['id'], 
-                                description=i['_source']['description'], 
-                                published=i['_source']['published']).save()
-        return render(request, 'vulners_list.html', context={'form': form})
+                try:
+                    obj = Vulner.objects.get(vulner=i['_source']['id'])
+                    print(obj)
+                except Vulner.DoesNotExist:
+                    obj = Vulner(
+                        vulner=i['_source']['id'],
+                        vendor=i['_source']['type'],
+                        description=i['_source']['description'],
+                        published=i['_source']['published'])
+                    obj.save()
+
+        return render(request, 'vulners_list.html', context={
+                                    'form': form,
+                                    'vulners_list': vln_lst,
+                                    'skip': skip})
