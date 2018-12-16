7917c9ad24921aed5a94aa2b36554930b2b25dda
YuriyB <paganism@mail.ru>
2018-12-16 22:05:18 +0300

    simplified code

--- vulners/vulners_engine/forms.py
+++ vulners/vulners_engine/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class FilterForm(forms.Form):
-    vendor = forms.CharField(required=False)
-    vulner = forms.CharField(required=False)
+    vendor = forms.CharField(required=False, label='Vendor')
+    vulner = forms.CharField(required=False, label='Vulner')
